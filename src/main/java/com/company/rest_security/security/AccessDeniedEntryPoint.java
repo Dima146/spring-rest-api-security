87f974f9679acebf85a94f4783aa9b1aa68e1a83
Dmitri Karpovich <dmitriykarpovich123321@gmail.com>
2023-11-18 22:54:27 +0300

    Update access denied entry point

--- src/main/java/com/company/rest_security/security/AccessDeniedEntryPoint.java
+++ src/main/java/com/company/rest_security/security/AccessDeniedEntryPoint.java
@@ -1,10 +1,13 @@
 package com.company.rest_security.security;
 
 import com.company.rest_security.exception.ErrorResponse;
+import com.company.rest_security.logging.LoggingAspect;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
@@ -19,10 +22,13 @@ import java.time.LocalDateTime;
 @Component
 public class AccessDeniedEntryPoint implements AccessDeniedHandler {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
+
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException)
             throws IOException, ServletException {
 
+        LOGGER.error("Access to resource denied", accessDeniedException);
         response.setContentType(MimeTypeUtils.APPLICATION_JSON_VALUE);
         response.setCharacterEncoding("UTF-8");
         response.setStatus(HttpStatus.FORBIDDEN.value());
@@ -30,7 +36,6 @@ public class AccessDeniedEntryPoint implements AccessDeniedHandler {
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.FORBIDDEN.toString(),
                                                         "You do not have permission to access this resource",
                                                         LocalDateTime.now());
-
         response.getWriter()
                 .write(String.valueOf(new ObjectMapper()
                         .writeValueAsString(errorResponse)));
